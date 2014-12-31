100036b6cad3abb363cc0d23b7e5a4f45f51875d
vanCopper <vancopperm@gmail.com>
2014-12-31 15:53:23 +0800

    + 添加新文章 页游中的配置管理

--- code/ConfigTool/ConfigTool.py
+++ code/ConfigTool/ConfigTool.py
@@ -1,12 +1,38 @@
+#coding: utf-8
 import xlrd
+import xlwt
+
 print("ConfigTool>>>>")
 book = xlrd.open_workbook("CS.xlsx")
-print "The number of worksheets is", book.nsheets
-print "Worksheet name(s):", book.sheet_names()
 sh = book.sheet_by_index(0)
-print sh.name, sh.nrows, sh.ncols
-print "Cell A11 is", sh.cell_value(rowx=0, colx=0)
-for rx in range(sh.nrows):
-    print sh.row(rx)
-# Refer to docs for more details.
-# Feedback on API is welcomed.
+
+
+serverBook = xlwt.Workbook(encoding='utf-8', style_compression=0)  
+serverSheet = serverBook.add_sheet('sheet', cell_overwrite_ok=True) 
+serverCol = 0
+
+clientBook = xlwt.Workbook(encoding='utf-8', style_compression=0)
+clientSheet = clientBook.add_sheet('sheet', cell_overwrite_ok=True)
+clientCol = 0
+
+for cx in range(sh.ncols):
+	cel = sh.col(cx)
+	# print(cel)
+	typeFlag = cel[0].value
+	# print typeFlag
+	if typeFlag == "CS" or typeFlag == "S":	
+		for vx in xrange(1, len(cel)):
+			v = cel[vx]
+			print vx
+			print v
+			serverSheet.write(vx - 1, serverCol, v.value)
+		serverCol += 1
+
+	if typeFlag == "CS" or typeFlag == "C":
+		for vc in xrange(1,len(cel)):
+			c = cel[vc]
+			clientSheet.write(vc - 1, clientCol, c.value)
+		clientCol += 1	
+
+serverBook.save("Server.csv")	
+clientBook.save("Client.csv")
