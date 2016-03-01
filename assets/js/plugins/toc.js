0b1be38fefac30407fba919e699c0970491d0554
vanCopper <vancopperm@gmail.com>
2016-03-01 17:26:26 +0800

    TOC

--- assets/js/plugins/toc.js
+++ assets/js/plugins/toc.js
@@ -44,45 +44,53 @@
 
     var get_level = function(ele) { return parseInt(ele.nodeName.replace("H", ""), 10); }
     var highest_level = headers.map(function(_, ele) { return get_level(ele); }).get().sort()[0];
-    var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
+    // var return_to_top = '<i class="icon-arrow-up back-to-top"> </i>';
 
     var level = get_level(headers[0]),
       this_level,
-      html = settings.title + " <"+settings.listType+">";
-    headers.on('click', function() {
-      if (!settings.noBackToTopLinks) {
-        window.location.hash = this.id;
-      }
-    })
-    .addClass('clickable-header')
-    .each(function(_, header) {
+      // html = " <"+settings.listType+">";//settings.title + 
+    html = "<p class='md-toc-content'>";
+    // headers.on('click', function() {
+    //   if (!settings.noBackToTopLinks) {
+    //     window.location.hash = this.id;
+    //   }
+    // })
+    // .addClass('clickable-header')
+    headers.each(function(_, header) {
       this_level = get_level(header);
-      if (!settings.noBackToTopLinks && this_level === highest_level) {
-        $(header).addClass('top-level-header').after(return_to_top);
-      }
-      if (this_level === level) // same level as before; same indenting
+      // if (!settings.noBackToTopLinks && this_level === highest_level) {
+      //   $(header).addClass('top-level-header').after(return_to_top);
+      // }
+      if (this_level === level)
+      {
+        // same level as before; same indenting
         html += "<li><a href='#" + fixedEncodeURIComponent(header.id) + "'>" + header.innerHTML + "</a>";
-      else if (this_level <= level){ // higher level than before; end parent ol
-        for(i = this_level; i < level; i++) {
+      }else if (this_level <= level)
+      { // higher level than before; end parent ol
+        for(i = this_level; i < level; i++) 
+        {
           html += "</li></"+settings.listType+">"
         }
         html += "<li><a href='#" + fixedEncodeURIComponent(header.id) + "'>" + header.innerHTML + "</a>";
-      }
-      else if (this_level > level) { // lower level than before; expand the previous to contain a ol
-        for(i = this_level; i > level; i--) {
+      }else if (this_level > level) 
+      { // lower level than before; expand the previous to contain a ol
+        for(i = this_level; i > level; i--) 
+        {
           html += "<"+settings.listType+"><li>"
         }
         html += "<a href='#" + fixedEncodeURIComponent(header.id) + "'>" + header.innerHTML + "</a>";
       }
       level = this_level; // update for the next one
     });
-    html += "</"+settings.listType+">";
-    if (!settings.noBackToTopLinks) {
-      $(document).on('click', '.back-to-top', function() {
-        $(window).scrollTop(0);
-        window.location.hash = '';
-      });
-    }
+
+    html += "</p>";
+    // html += "</"+settings.listType+">";
+    // if (!settings.noBackToTopLinks) {
+    //   $(document).on('click', '.back-to-top', function() {
+    //     $(window).scrollTop(0);
+    //     window.location.hash = '';
+    //   });
+    // }
 
     render[settings.showEffect]();
   };
